142b3a50b6400781497988fcce4b87fa109eae92
David Amrani <david@amrani.io>
2017-03-31 14:22:33 -0400

    spacing issues test

--- lib/operations/basic_pack.rb
+++ lib/operations/basic_pack.rb
@@ -1,25 +1,32 @@
 module Operations
-	module BasicPack
-		def op__append s = ""
-			w = @content + s.to_s
-			update w
-		end
-		def op__delete k
-			idx = (k.to_i * -1) - 1
-			s = @content[0..idx]
-			update s
-		end
-		def op__print k
-			puts @content[k.to_i - 1]
-		end
-		def op__undo
-			@content = @history.pop || ""
-			@content
-		end
-	private
-		def update s
-			@history << @content
-			@content = s
-		end
-	end
+  module BasicPack
+
+    def op__append s = ""
+      w = @content + s.to_s
+      update w
+    end
+
+    def op__delete k
+      idx = (k.to_i * -1) - 1
+      s = @content[0..idx]
+      update s
+    end
+
+    def op__print k
+      puts @content[k.to_i - 1]
+    end
+
+    def op__undo
+      @content = @history.pop || ""
+      @content
+    end
+
+    private
+
+    def update s
+      @history << @content
+      @content = s
+    end
+
+  end
 end
